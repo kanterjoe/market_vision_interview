70289c3fb13552479c74e3747208fcf78d6b04ed
Joe Kanter (From Mac) <kanterjoe@gmail.com>
2019-05-15 23:22:22 -0500

    Fixed sendfile

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const path   = require('path');
 
 
+router.get("*", (req,res,err) => res.sendFile(path.join(path.resolve(path.dirname('')) ,"client/build")))
+router.get("/", (req,res,err) => res.sendFile(path.join(path.resolve(path.dirname('')) ,"client/build")))
+
 router.use("/user", require('./user'));
 router.use("/cart", require('./cart'));
 router.use("/product", require('./product'));
 
-router.get("*", (req,res,err) => res.sendFile(path.join(path.resolve(path.dirname('')) ,"client/build")))
-router.get("/", (req,res,err) => res.sendFile(path.join(path.resolve(path.dirname('')) ,"client/build")))
 
 module.exports = router;
